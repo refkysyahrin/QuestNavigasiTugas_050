10aafea86c9ebd13482dd938d074cdef4dc19623
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-11-09 20:12:28 +0700

    membuat fun DaftarPeserta

--- app/src/main/java/com/example/praktikum6pam/view/DaftarPeserta.kt
+++ app/src/main/java/com/example/praktikum6pam/view/DaftarPeserta.kt
@@ -1,2 +1,15 @@
 package com.example.praktikum6pam.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import java.lang.reflect.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DaftarPeserta(
+    onBeranda: () -> Unit,
+    onForm: () -> Unit,
+    modifier: Modifier = Modifier
+){
+
+}

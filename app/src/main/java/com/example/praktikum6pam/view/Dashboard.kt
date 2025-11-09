dc835e0cc237d78da4d291572bdf1afd04c00b93
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-11-09 19:52:23 +0700

    Menambahkan gambar dari drawable

--- app/src/main/java/com/example/praktikum6pam/view/Dashboard.kt
+++ app/src/main/java/com/example/praktikum6pam/view/Dashboard.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -42,6 +43,10 @@ fun Dashboard(
             fontSize = 25.sp)
 
         Spacer(modifier = Modifier.height(10.dp))
+
+        // Gambar halaman utama
+        val gambar1 = painterResource(id = R.drawable.logosmandatas)
+
 
 
 

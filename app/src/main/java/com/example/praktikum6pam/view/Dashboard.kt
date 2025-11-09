5153bd3d98c96b14792989f8f3c65e0b0e02a370
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-11-09 19:37:38 +0700

    Membuat fun Dashboard

--- app/src/main/java/com/example/praktikum6pam/view/Dashboard.kt
+++ app/src/main/java/com/example/praktikum6pam/view/Dashboard.kt
@@ -1,2 +1,10 @@
 package com.example.praktikum6pam.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Dashboard(
+
+)

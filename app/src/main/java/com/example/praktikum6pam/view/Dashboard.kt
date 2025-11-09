0f06d69870af22f379ccc57cdfbdf0968554597a
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-11-09 19:44:49 +0700

    Inisialisasi layout dan state untuk layar

--- app/src/main/java/com/example/praktikum6pam/view/Dashboard.kt
+++ app/src/main/java/com/example/praktikum6pam/view/Dashboard.kt
@@ -1,10 +1,32 @@
 package com.example.praktikum6pam.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Dashboard(
+    onNavigate: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    var isloading by remember { mutableStateOf(false) }
+    val scope = rememberCoroutineScope()
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .padding(top = 150.dp),
+        horizontalAlignment = Alignment.CenterHorizontally)
+    {
 
-)
+    }
+}

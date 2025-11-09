86216df45fe04b67d730c4622acc9cde32d9735d
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-11-09 19:26:24 +0700

    Menambahkan lib navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,12 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
+    //
+    implementation("com.valentinilk.shimmer:compose-shimmer:1.3.3")
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.compose.foundation)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)

9fc95fe68b3e6f4e59f9cd04d83990e82d5a3a23
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-11-09 19:49:32 +0700

    Memasukan teks welcome

--- app/src/main/java/com/example/praktikum6pam/view/Dashboard.kt
+++ app/src/main/java/com/example/praktikum6pam/view/Dashboard.kt
@@ -1,9 +1,12 @@
 package com.example.praktikum6pam.view
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -12,7 +15,10 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.praktikum6pam.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -27,6 +33,10 @@ fun Dashboard(
         .padding(top = 150.dp),
         horizontalAlignment = Alignment.CenterHorizontally)
     {
+        Text(text = stringResource(id = R.string.welcome),
+            fontSize = 40.sp)
+
+        Spacer(modifier = Modifier.height(10.dp))
 
     }
 }

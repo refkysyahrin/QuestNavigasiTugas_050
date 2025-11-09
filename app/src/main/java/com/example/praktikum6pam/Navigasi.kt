859e99c6a0b93b3f25bb20b1342d9bd55a57f7b2
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-11-09 20:49:35 +0700

    Tombol di dashboard bisa langsung navigasi ke daftar

--- app/src/main/java/com/example/praktikum6pam/Navigasi.kt
+++ app/src/main/java/com/example/praktikum6pam/Navigasi.kt
@@ -1,8 +1,33 @@
 package com.example.praktikum6pam
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.example.praktikum6pam.view.Dashboard
+import java.lang.reflect.Modifier
+
 enum class Navigasi{
     Dashboard,
     DaftarPeserta,
     Formulir
+}
+
+@Composable
+fun DataApp(
+    modifier: Modifier = Modifier,
+    navController: NavController = rememberNavController()
+){
+    NavHost(
+        navController = navController,
+        startDestination = Navigasi.Dashboard.name,
+        modifier = Modifier.padding()
+    ){
+        composable(route = Navigasi.Dashboard.name) {
+            Dashboard(
+                //Tombol di dashboard bisa langsung navigasi ke daftar
+            )
+        }
+    }
 }
 
